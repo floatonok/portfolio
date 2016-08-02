74ae0bc4f1067576212e9b75d6ac11ea883a5a44
Isabella Ong <isabellaong@gmail.com>
2016-08-03 01:05:12 +0800

    adds chartjs graphs for skills

--- app/components/portfolio-skills.js
+++ app/components/portfolio-skills.js
@@ -1,4 +1,62 @@
-import Ember from 'ember';
+import Ember from 'ember'
 
 export default Ember.Component.extend({
-});
+  numberData: Ember.computed('model', function () {
+    return {
+      labels: this.get('skills').mapBy('skill'),
+      datasets: [{
+        label: 'Skills',
+        data: this.get('skills').mapBy('proficiency'),
+        backgroundColor: '#F2385A'
+      }]
+    }
+  }),
+
+  numberData2: Ember.computed('model', function () {
+    return {
+      labels: this.get('skills').mapBy('skill'),
+      datasets: [{
+        label: 'Skills',
+        data: this.get('skills').mapBy('proficiency'),
+        backgroundColor: '#000'
+      }]
+    }
+  }),
+
+  // skillsArray: function () {
+  //   return this.get('skills').map(function (item) {
+  //     return item.getProperties('skill')
+  //   })
+  // }.property('skill.[]'),
+
+  // proficiencyArray: function () {
+  //   return this.get('skills').map(function (item) {
+  //     return item.getProperties('proficiency')
+  //   })
+  // }.property('proficiency.[]'),
+
+  didRender: function () {
+    var ctx = document.getElementById('myChart')
+    var myChart = new Chart(ctx, {
+      type: 'bar',
+      data: this.get('numberData'),
+      options: {
+        responsive: true,
+        legend: {
+          display: false
+        }
+      }
+    })
+    var ctx = document.getElementById('myChart2')
+    var myChart2 = new Chart(ctx, {
+      type: 'bar',
+      data: this.get('numberData2'),
+      options: {
+        responsive: true,
+        legend: {
+          display: false
+        }
+      }
+    })
+  }
+})
