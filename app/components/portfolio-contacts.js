078ed75bd268154d11e4a1b306a5610ba7b87049
Isabella Ong <isabellaong@gmail.com>
2016-08-04 00:38:27 +0800

    adds vivus animation for all icons

--- app/components/portfolio-contacts.js
+++ app/components/portfolio-contacts.js
@@ -1,4 +1,7 @@
 import Ember from 'ember'
 
 export default Ember.Component.extend({
+  didRender: function () {
+    new Vivus('Layer_6', {duration: 100})
+  }
 })
--- app/components/portfolio-educations.js
+++ app/components/portfolio-educations.js
@@ -2,6 +2,8 @@ import Ember from 'ember'
 
 export default Ember.Component.extend({
   didRender: function () {
+    new Vivus('Layer_4', {duration: 100})
+    new Vivus('Layer_5', {duration: 100})
     new WOW().init()
   }
 })
