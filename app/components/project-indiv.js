c11453be9dba98f41eb5d9217aef28df19ef8acc
Isabella Ong <isabellaong@gmail.com>
2016-08-03 16:18:33 +0800

    adds individual modal for each project and styling for education page

--- app/components/project-indiv.js
+++ app/components/project-indiv.js
@@ -2,6 +2,6 @@ import Ember from 'ember'
 
 export default Ember.Component.extend({
   classNames: ['col-md-3', 'col-xs-12', 'mix', 'webdev']
-  // attributeBindings: ['data-myorder'],
-  // data-myorder: null
+  // attributeBindings: ['style'],
+  // style: 'height: 100%; border: 2px dotted black;'
 })
