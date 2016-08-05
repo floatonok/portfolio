56b1afea7a2c751e64d2217eb59deb7d60d207f9
Isabella Ong <isabellaong@gmail.com>
2016-08-05 11:03:20 +0800

    hmm

--- app/components/project-indiv.js
+++ app/components/project-indiv.js
@@ -1,7 +1,7 @@
 import Ember from 'ember'
 
 export default Ember.Component.extend({
-  classNames: ['col-md-3', 'col-xs-12', 'mix', 'webdev']
-  // attributeBindings: ['style'],
-  // style: 'height: 100%; border: 2px dotted black;'
+  // classNames: ['col-md-3', 'col-xs-12', 'mix', 'webdev']
+  // attributeBindings: ['data-myorder'],
+  // data-myorder:
 })
--- app/models/project.js
+++ app/models/project.js
@@ -2,7 +2,9 @@ import DS from 'ember-data'
 
 export default DS.Model.extend({
   title: DS.attr('string'),
+  category: DS.attr('string'),
   description: DS.attr('string'),
+  dataOrder: DS.attr('number'),
   links: DS.attr(),
   images: DS.attr(),
   skills: DS.attr()
