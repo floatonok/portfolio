b902195fa27f66f0733be964eaeb1fcebc66add4
Isabella Ong <isabellaong@gmail.com>
2016-08-22 17:12:09 +0800

    tidies up code

--- app/components/project-indiv.js
+++ app/components/project-indiv.js
@@ -1,7 +1,4 @@
 import Ember from 'ember'
 
 export default Ember.Component.extend({
-  // classNames: ['col-md-3', 'col-xs-12', 'mix', 'webdev']
-  // attributeBindings: ['data-myorder'],
-  // data-myorder:
 })
